e34af27e784a86bcd7cd5fa107496c218490bd33
SoccyUwU <socc.pupet77943@gmail.com>
2025-07-10 02:41:08 -0400

    Forceaging works

--- common/src/main/kotlin/io/github/soccyuwu/pregxxy/registry/PregxxyActions.kt
+++ common/src/main/kotlin/io/github/soccyuwu/pregxxy/registry/PregxxyActions.kt
@@ -7,6 +7,7 @@ import at.petrak.hexcasting.api.casting.math.HexPattern
 import at.petrak.hexcasting.common.lib.HexRegistries
 import at.petrak.hexcasting.common.lib.hex.HexActions
 import io.github.soccyuwu.pregxxy.casting.actions.spells.OpBreed
+import io.github.soccyuwu.pregxxy.casting.actions.spells.OpNurture
 import io.github.soccyuwu.pregxxy.casting.actions.spells.great.OpGreaterBreed
 
 object PregxxyActions : PregxxyRegistrar<ActionRegistryEntry>(
@@ -15,6 +16,7 @@ object PregxxyActions : PregxxyRegistrar<ActionRegistryEntry>(
 ) {
     val BREED = make("pregxxy", HexDir.SOUTH_EAST, "wdwewawqwqwwaqwq", OpBreed)
     val GBREED = make("greater_pregxxy", HexDir.SOUTH_EAST, "wwdwwewwawwqwwqwwwawqwqqadadaaead", OpGreaterBreed)
+    val NURTURE = make("nurture", HexDir.SOUTH_EAST, "weaqawwwaqawwawqwqw", OpNurture)
 
     private fun make(name: String, startDir: HexDir, signature: String, action: Action) =
         make(name, startDir, signature) { action }
