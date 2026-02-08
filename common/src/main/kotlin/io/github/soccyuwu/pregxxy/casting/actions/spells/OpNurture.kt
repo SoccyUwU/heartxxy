e7f7b86ef5320459a4e3d60fd39d69779ec32ffb
SoccyUwU <socc.pupet77943@gmail.com>
2026-02-07 20:43:12 -0500

    Nurture changed to mimic feeding and cost nerfed.

--- common/src/main/kotlin/io/github/soccyuwu/pregxxy/casting/actions/spells/OpNurture.kt
+++ common/src/main/kotlin/io/github/soccyuwu/pregxxy/casting/actions/spells/OpNurture.kt
@@ -4,35 +4,32 @@ import at.petrak.hexcasting.api.casting.ParticleSpray
 import at.petrak.hexcasting.api.casting.RenderedSpell
 import at.petrak.hexcasting.api.casting.castables.SpellAction
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
-import at.petrak.hexcasting.api.casting.getDouble
 import at.petrak.hexcasting.api.casting.getEntity
 import at.petrak.hexcasting.api.casting.iota.Iota
 import at.petrak.hexcasting.api.misc.MediaConstants
 import io.github.soccyuwu.pregxxy.casting.mishaps.MishapCantNurture
 import net.minecraft.world.entity.AgeableMob
-import kotlin.math.pow
 
 object OpNurture : SpellAction {
     override val argc = 2
 
     override fun execute(args: List<Iota>, env: CastingEnvironment): SpellAction.Result {
         val target = args.getEntity(0, argc)
-        val ageMod = args.getDouble(1, argc)
         env.assertEntityInRange(target)
         if(target !is AgeableMob){
             throw MishapCantNurture(target)
         }
 
         return SpellAction.Result(
-            Spell(target, ageMod),
-            (((ageMod / 200).pow(2) + ageMod/100 + 2) * MediaConstants.DUST_UNIT).toLong(),
+            Spell(target),
+            2 * MediaConstants.DUST_UNIT,
             listOf(ParticleSpray.cloud(target.position().add(0.0, target.eyeHeight / 2.0, 0.0), 1.0))
         )
     }
 
-    private data class Spell(val target: AgeableMob, val ageMod: Double) : RenderedSpell {
+    private data class Spell(val target: AgeableMob) : RenderedSpell {
         override fun cast(env: CastingEnvironment) {
-            target.age += ageMod.toInt()
+            target.ageUp(AgeableMob.getSpeedUpSecondsWhenFeeding(-target.age), true)
         }
     }
 }
